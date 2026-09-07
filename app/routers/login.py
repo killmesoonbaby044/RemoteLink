6b3e0892e18853f40d70ad93af2325dd0b427208
killmesoonbaby044 <alexandr.lanovoy@gmail.com>
2026-09-07 03:04:13 +0300

    - fix status code check and logging

--- app/routers/login.py
+++ app/routers/login.py
@@ -19,7 +19,7 @@ async def login(
 ):
     config = get_config()
     logger.info(
-        f" From {request.client.host} trying to login with login {login_user.username!r} -> via LDAP"
+        f" From {request.client.host!r} trying to login with login {login_user.username!r} -> via LDAP"
     )
 
     resp: httpx.Response = await request.app.state.http_client.post(
@@ -31,9 +31,19 @@ async def login(
     )
 
     if resp.status_code >= 400:
+
+        content_type = resp.headers.get("content-type", "")
+
+        if content_type.startswith("application/json"):
+            detail = resp.json().get("detail")
+        else:
+            detail = resp.text
+
         raise HTTPException(
-            status_code=resp.status_code, detail=resp.json().get("detail")
+            status_code=resp.status_code,
+            detail=detail,
         )
+
     token = resp.json()["access_token"]
     response.set_cookie(
         token_name.access_token,
@@ -42,6 +52,6 @@ async def login(
         samesite="strict",
         max_age=config.jwt_expires,
     )
-    logger.info(f"User {login_user.username!r} -> {request.client.host} logged in")
+    logger.info(f"User {login_user.username!r} -> {request.client.host!r} logged in")
 
     return {"message": "ok"}
